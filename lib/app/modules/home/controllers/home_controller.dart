996c186caafc65da5d626d84b37ef6aef75d5c5f
Faqihasyari <faqih8158@gmail.com>
2025-05-06 23:19:43 +0700

    logout

--- lib/app/modules/home/controllers/home_controller.dart
+++ lib/app/modules/home/controllers/home_controller.dart
@@ -1,5 +1,16 @@
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:get/get.dart';
+import 'package:myfirebase/app/routes/app_pages.dart';
 
 class HomeController extends GetxController {
-  
+  FirebaseAuth auth = FirebaseAuth.instance;
+
+  void logout() async {
+    try {
+      await auth.signOut();
+      Get.offAllNamed(Routes.LOGIN);
+    } catch (e) {
+      print(e);
+    }
+  }
 }
