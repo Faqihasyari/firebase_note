b1026140a4faafb78a42e28cf0753f5c0181f576
Faqihasyari <faqih8158@gmail.com>
2025-05-11 23:44:51 +0700

    final

--- lib/app/modules/reset_password/controllers/reset_password_controller.dart
+++ lib/app/modules/reset_password/controllers/reset_password_controller.dart
@@ -1,5 +1,21 @@
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:flutter/widgets.dart';
 import 'package:get/get.dart';
 
 class ResetPasswordController extends GetxController {
-  
+  RxBool isLoading = false.obs;
+  TextEditingController emailC = TextEditingController();
+
+  FirebaseAuth auth = FirebaseAuth.instance;
+
+  void reset()async{
+    if(emailC.text.isNotEmpty){
+      isLoading.value = true;
+    await auth.sendPasswordResetEmail(email: emailC.text);
+      Get.back();
+      Get.snackbar("BERHASIL", "Kami telah mengirimkan email ke alamat email Anda");
+      isLoading.value = false;
+
+    }
+  }
 }
