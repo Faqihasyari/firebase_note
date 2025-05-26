bbdc5b27af3c4af555cd6357d935b5d92253514f
Faqihasyari <faqih8158@gmail.com>
2025-05-26 16:31:12 +0700

    picker

--- lib/main.dart
+++ lib/main.dart
@@ -6,6 +6,7 @@ import 'package:get/get.dart';
 import 'package:get_storage/get_storage.dart';
 import 'package:myfirebase/app/modules/splash/splash.dart';
 import 'package:myfirebase/firebase_options.dart';
+import 'package:supabase_flutter/supabase_flutter.dart' as supabase;
 
 import 'app/routes/app_pages.dart';
 
@@ -14,21 +15,27 @@ void main() async {
 
   await Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform);
 
+  await supabase.Supabase.initialize(
+      url: 'https://dabyallnszwtlhqypgpd.supabase.co',
+      anonKey:
+          'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImRhYnlhbGxuc3p3dGxocXlwZ3BkIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NDgyNTAwMTAsImV4cCI6MjA2MzgyNjAxMH0.JvlAwCp6XvzqR--3nS5tXoW7kjYFWYQWY-L9Hr697x0');
+
   FirebaseAuth auth = FirebaseAuth.instance;
   await GetStorage.init();
   runApp(
-    StreamBuilder <User?>(
-      stream: auth.authStateChanges(),
-      builder: (context, snap) {
-        if (snap.connectionState == ConnectionState.waiting){
-          return SplashScreen();
-        }
-        return GetMaterialApp(
-          title: "Application",
-          initialRoute: snap.data != null && snap.data!.emailVerified == true ? Routes.LOGIN : Routes.LOGIN,
-          getPages: AppPages.routes,
-        );
-      }
-    ),
+    StreamBuilder<User?>(
+        stream: auth.authStateChanges(),
+        builder: (context, snap) {
+          if (snap.connectionState == ConnectionState.waiting) {
+            return SplashScreen();
+          }
+          return GetMaterialApp(
+            title: "Application",
+            initialRoute: snap.data != null && snap.data!.emailVerified == true
+                ? Routes.LOGIN
+                : Routes.LOGIN,
+            getPages: AppPages.routes,
+          );
+        }),
   );
 }
